8c502b820ab5383d85dd05061ef282900103c2e6
Keith Sabine <tetractys4321@protonmail.com>
2021-09-24 18:06:21 -0600

    more output

--- sniffspoof/sniffer.py
+++ sniffspoof/sniffer.py
@@ -6,25 +6,29 @@ from scapy.all import *
 def print_pkt(pkt):
     pkt.show()
 
-# This default print_pkt is ugly lets use some formatting from 
+# pkt.show() is giving us too much information for testing out
+# filters. Lets use some formatting from 
 # https://scapy.readthedocs.io/en/latest/usage.html#reading-pcap-files
 def pprint_pkt(pkt):
     # Print out showing Origin IP:port -> Dest IP:port
     print(pkt[IP].src + "->" + pkt[IP].dst)
 
 # Demo the sniffer on our container network interface
-# pkt = sniff(iface='br-8928c17f4ab4' , filter='icmp', prn=print_pkt)
+# pkt = sniff(iface='br-0229d0abdb25' , filter='icmp', prn=print_pkt)
 
 #Task 1.1B --- Capturing only the ICMP packets
 def icmp_only():
-    pkt = sniff(iface='br-8928c17f4ab4' , filter='icmp', prn=pprint_pkt)
+    print("CAPTURING ICMP -------------\n")
+    pkt = sniff(iface='br-0229d0abdb25' , filter='icmp', prn=pprint_pkt)
 
 #Task 1.1B --- Capturing only TCP packets going to port 23
 def tcp_port23():
-    pkt = sniff(iface='br-8928c17f4ab4', filter='tcp and host 9.9.9.9 and port 23', prn=pprint_pkt)
+    print("CAPTURING TCP PORT 23 HOST 9.9.9.9 -------------\n")
+    pkt = sniff(iface='br-0229d0abdb25', filter='tcp and host 9.9.9.9 and port 23', prn=pprint_pkt)
 
 #Task 1.1B --- Capturing packets FROM a subnet
 def tofrom_subnet():
+    print("CAPTURING 1.0.0.0/8 PACKETS")
     pkt = sniff(iface='enp0s3', filter='net 1.0.0.0 mask 255.0.0.0', count=1,prn=pprint_pkt)
 
 ################################################################################
